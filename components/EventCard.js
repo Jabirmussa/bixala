d9c4c4222d1f018b55431c1944a96a6516bd8e2c
Growskills <jabirmussa185@gmail.com>
2025-02-04 15:27:25 +0200

    fix: card hight

--- components/EventCard.js
+++ components/EventCard.js
@@ -2,14 +2,12 @@
 import Image from "next/image";
 export default function EventCard() {
     return (
-        <div className="card bg-base-100 shadow-xl">
-            <div className="card-body">
+        <div className="card bg-base-100 shadow-xl rounded-xl">
                 <Image src="/cover.jpg" alt="image" width={400} height={200} />
-                <h2 className="card-title">Bixa para fulano</h2>
-                <h3>30/10/2025 <span>Mes & dia</span></h3>
-                <div className="card-actions justify-end">
-                    <a href={"/event/1"} className="btn btn-primary">Markar presença</a>
-                </div>
+            <div className="card-body p-5">
+                <h2 className="card-title text-xl font-bold mb-8">Bixa para fulano</h2>
+                <h3 className="text-base mb-5">30/10/2025 <span>Mes & dia</span></h3>
+                <a href={"/event/1"} className="btn btn-primary">Markar presença</a>
             </div>
         </div>
     );
