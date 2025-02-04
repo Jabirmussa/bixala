a76a384f54104b55d7518c5329b02273b974fc45
Growskills <jabirmussa185@gmail.com>
2025-02-04 15:43:29 +0200

    feat: botton handling event

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+'use client'
 import Image from "next/image";
 import EventCard from "@/components/EventCard";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Calendar } from "@/components/ui/calendar"
+
 
 export default function Home() {
   return (
@@ -13,13 +16,33 @@ export default function Home() {
         className="h-full w-full rounded-md object-cover"
       />
     </AspectRatio>
-    <div className="grid self-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 lg:gap-12">
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
+    <div>
+      <div className="relative flex items-center justify-between mb-5">
+         <h2 className="text-3xl font-bold mb-8">Bixas disponíveis</h2>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => {
+            const calendar = document.querySelector(".calendar");
+            if (calendar) {
+              calendar.classList.toggle("hidden");
+            }
+          }}
+        >
+          Ver calendário
+        </button>
+        <div className="hidden calendar absolute top-0 right-50 bg-white p-4 rounded-md shadow-md z-50">
+          <Calendar  />
+        </div>
+      </div>
+      <div className="grid self-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 lg:gap-12">
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+      </div>
     </div>
     </>
   );
